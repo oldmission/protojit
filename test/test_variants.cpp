87d067725ebf91287e4f299cadb1e24c39f7dfd0
Kapil Kanwar <kapil.kanwar@oldmissioncapital.com>
2022-04-08 16:24:09 -0500

    Update int, struct, and variant tests for new flow

--- test/test_variants.cpp
+++ test/test_variants.cpp
@@ -8,231 +8,151 @@
 
 namespace pj {
 
-TEST_F(PJTest, VariantSame) {
+TEST_P(PJVariantTest, VariantSame) {
   Var1 F{.value = {.x = 42}, .tag = Var1::Kind::x};
-  Var1 T{.value = {.x = 0}, .tag = Var1::Kind::undef};
+  Var1 T{.value = {.x = 0xffffffff}, .tag = static_cast<Var1::Kind>(0x7f)};
 
-  OnMatch<0, Var1>("x", [&](const Var1& T) {
+  onMatch<0, Var1>("x", [&](const Var1& T) {
     EXPECT_EQ(T.value.x, 42);
     EXPECT_EQ(T.tag, Var1::Kind::x);
   });
 
-  Transcode<Var1>(&F, &T, {"x"}, {"."}, {"."});
+  transcode<Var1>(&F, &T, "x", "_");
 
   EXPECT_EQ(F.value.x, 42);
 }
 
-TEST_F(PJTest, VariantSameNoTag) {
-  Var1 F{.value = {.x = 42}, .tag = Var1::Kind::x};
-  Var1 T{.value = {.x = 0xffffffff}, .tag = static_cast<Var1::Kind>(0x7f)};
-
-  Transcode<Var1>(&F, &T, {"x"});
-
-  EXPECT_EQ(F.tag, Var1::Kind::x);
-  EXPECT_EQ(T.value.x, 42);
-  EXPECT_EQ(T.tag, Var1::Kind::x);
-}
-
-TEST_F(PJTest, VariantSameNoPath) {
-  Var1 F{.value = {.x = 42}, .tag = Var1::Kind::x};
-  Var1 T{.value = {.x = 0xffffffff}, .tag = static_cast<Var1::Kind>(0x7f)};
-
-  Transcode<Var1>(&F, &T);
-
-  EXPECT_EQ(F.tag, Var1::Kind::x);
-  EXPECT_EQ(T.value.x, 42);
-  EXPECT_EQ(T.tag, Var1::Kind::x);
-}
-
-TEST_F(PJTest, VariantMismatchNoTag) {
+TEST_P(PJVariantTest, VariantMismatch) {
   Var1 F{.value = {.x = 42}, .tag = Var1::Kind::x};
   Var2 T{.value = {.y = -1}, .tag = Var2::Kind::y};
 
-  OnNoMatch<0, Var2>("y", [&](const Var2& T) {});
+  onNoMatch<0, Var2>("y", [&](const Var2& T) {});
+  onMatch<1, Var2>("undef", [&](const Var2& T) {});
 
-  Transcode<Var1, Var2>(&F, &T, {"x"});
+  transcode<Var1, Var2>(&F, &T, "x", "_");
 
   EXPECT_EQ(T.tag, Var2::Kind::undef);
 }
 
-TEST_F(PJTest, VariantMismatch) {
-  Var1 F{.value = {.x = 42}, .tag = Var1::Kind::x};
-  Var2 T{.value = {.y = -1}, .tag = Var2::Kind::y};
-
-  OnNoMatch<0, Var2>("y", [&](const Var2& T) {});
-  OnMatch<0, Var2>("undef", [&](const Var2& T) {});
-
-  Transcode<Var1, Var2>(&F, &T, {"x"}, {"."}, {"."});
-
-  EXPECT_EQ(T.tag, Var2::Kind::undef);
-}
-
-TEST_F(PJTest, VariantAddCaseBigNoTag) {
-  Var1 F{.value = {.x = 42}, .tag = Var1::Kind::x};
-  Var3 T{.value = {.x = -1}, .tag = Var3::Kind::undef};
-
-  Transcode<Var1, Var3>(&F, &T, {"x"});
-
-  EXPECT_EQ(T.tag, Var3::Kind::x);
-  EXPECT_EQ(T.value.x, 42);
-}
-
-TEST_F(PJTest, VariantAddCaseBig) {
+TEST_P(PJVariantTest, VariantAddCaseBig) {
   Var1 F{.value = {.x = 42}, .tag = Var1::Kind::x};
   Var3 T{.value = {.x = 0}, .tag = Var3::Kind::undef};
 
-  OnMatch<0, Var3>("x", [&](const Var3& T) {
+  onMatch<0, Var3>("x", [&](const Var3& T) {
     EXPECT_EQ(T.value.x, 42);
     EXPECT_EQ(T.tag, Var3::Kind::x);
   });
 
-  Transcode<Var1, Var3>(&F, &T, {"x"}, {"."}, {"."});
+  transcode<Var1, Var3>(&F, &T, "x", "_");
 
   EXPECT_EQ(F.value.x, 42);
 
-  // Ensure the tag is added -- message size shouldn't include
-  // BigStruct.
-  EXPECT_EQ((GenSize<Var1, Var1>(0x1000, {"x"}, {"."})(&F)), 9);
+  // TODO: GenSize does not yet exist
+  // Ensure the tag is added -- message size shouldn't include BigStruct.
+  // EXPECT_EQ((GenSize<Var1, Var1>(0x1000, {"x"}, {"."})(&F)), 9);
 }
 
-TEST_F(PJTest, VariantMoveCaseNoTag) {
+TEST_P(PJVariantTest, VariantMoveCase) {
   Var1 F{.value = {.x = 42}, .tag = Var1::Kind::x};
   Var4 T{.value = {.x = -1}, .tag = Var4::Kind::undef};
 
-  Transcode<Var1, Var4>(&F, &T, {"x"});
-
-  EXPECT_EQ(T.tag, Var4::Kind::x);
-  EXPECT_EQ(T.value.x, 42);
-}
-
-TEST_F(PJTest, VariantMoveCase) {
-  Var1 F{.value = {.x = 42}, .tag = Var1::Kind::x};
-  Var4 T{.value = {.x = -1}, .tag = Var4::Kind::undef};
-
-  OnMatch<0, Var4>("x", [&](const Var4& T) {
+  onMatch<0, Var4>("x", [&](const Var4& T) {
     EXPECT_EQ(T.tag, Var4::Kind::x);
     EXPECT_EQ(T.value.x, 42);
   });
 
-  Transcode<Var1, Var4>(&F, &T, {"x"}, {"."}, {"."});
+  transcode<Var1, Var4>(&F, &T, "x", "_");
 }
 
-TEST_F(PJTest, VariantMoveCase2) {
+// TODO: TranscodeOp not yet implemented for non-primitives
+#if 0
+TEST_P(PJVariantTest, VariantMoveCase2) {
   Var3 F{.value = {.x = 42}, .tag = Var3::Kind::x};
   Var4 T{.value = {.x = -1}, .tag = Var4::Kind::undef};
 
-  OnMatch<0, Var4>("x", [&](const Var4& T) {
+  onMatch<0, Var4>("x", [&](const Var4& T) {
     EXPECT_EQ(T.tag, Var4::Kind::x);
     EXPECT_EQ(T.value.x, 42);
   });
 
-  Transcode<Var3, Var4>(&F, &T, {"x"}, {"."}, {"."});
+  transcode<Var3, Var4>(&F, &T, "x", ".");
 
-  EXPECT_EQ((GenSize<Var3, Var4>(0x1000, {"x"}, {"."})(&F)), 9);
+  // TODO: GenSize does not yet exist
+  // EXPECT_EQ((GenSize<Var3, Var4>(0x1000, {"x"}, {"."})(&F)), 9);
 }
+#endif
 
+// TODO: implement DefaultOp lowering
+#if 0
 TEST_F(PJTest, VariantAddTagField) {
   Outer2 F{.z = 0xab};
   Outer T{.v = {.tag = Var4::Kind::undef}, .z = 0};
 
-  Transcode<Outer2, Outer, Outer>(&F, &T, {}, {"v", "."}, {});
+  transcode<Outer2, Outer>(&F, &T);
 
   EXPECT_EQ(T.v.tag, Var4::Kind::undef);
   EXPECT_EQ(T.z, 0xab);
 }
+#endif
 
-// TODO: try this without path, when supported
-TEST_F(PJTest, VariantRemoveTagField) {
+TEST_P(PJVariantTest, VariantRemoveTagField) {
   Outer F{.v = {.tag = Var4::Kind::undef}, .z = 0xab};
   Outer2 T{.z = 0};
 
-  Transcode<Outer, Outer2, Outer>(&F, &T, {"v", "undef"}, {"v", "."}, {});
+  transcode<Outer, Outer2, Outer>(&F, &T, "v.undef", "v._");
 
   EXPECT_EQ(T.z, 0xab);
 }
 
-TEST_F(PJTest, VariantSameNestedPathNoTag) {
-  Outer F{.v = Var4{.value = {.x = 42}, .tag = Var4::Kind::x}};
-  Outer T{.v = Var4{.value = {.x = -1}, .tag = Var4::Kind::undef}};
-
-  Transcode<Outer>(&F, &T, {"v", "x"});
-
-  EXPECT_EQ(F.v.tag, Var4::Kind::x);
-  EXPECT_EQ(F.v.value.x, 42);
-}
-
-// TODO: Enable this test when SizeTarget supports variants.
-#if 0
-TEST_F(PJTest, VariantSameNoPathNestedTag) {
-  Outer F{.v = Var4{.value = {.x = 42}, .tag = Var4::Kind::x}};
-  Outer T{.v = Var4{.value = {.x = -1}, .tag = Var4::Kind::undef}};
-
-  OnMatch<0, Outer>("x", [&](const Outer& T) {
-    EXPECT_EQ(T.v.tag, Var4::Kind::x);
-    EXPECT_EQ(T.v.value.x, 42);
-  });
-
-  Transcode<Outer>(&F, &T, {}, {"v", "."}, {"v", "."});
-}
-#endif
-
-TEST_F(PJTest, VariantSameNestedPathNestedTag) {
-  Outer F{.v = Var4{.value = {.x = 42}, .tag = Var4::Kind::x}};
-  Outer T{.v = Var4{.value = {.x = -1}, .tag = Var4::Kind::undef}};
-
-  OnMatch<0, Outer>("x", [&](const Outer& T) {
-    EXPECT_EQ(T.v.tag, Var4::Kind::x);
-    EXPECT_EQ(T.v.value.x, 42);
-  });
-
-  Transcode<Outer>(&F, &T, {"v", "x"}, {"v", "."}, {"v", "."});
-}
-
-TEST_F(PJTest, VariantSameNestedPathNestedTagAddFieldAfter) {
+TEST_P(PJVariantTest, VariantSameNestedPath) {
   Outer F{.v = Var4{.value = {.x = 42}, .tag = Var4::Kind::x}, .z = 0xab};
   Outer T{.v = Var4{.value = {.x = -1}, .tag = Var4::Kind::undef}, .z = 0};
 
-  OnMatch<0, Outer>("x", [&](const Outer& T) {
+  onMatch<0, Outer>("v.x", [&](const Outer& T) {
     EXPECT_EQ(T.v.tag, Var4::Kind::x);
     EXPECT_EQ(T.v.value.x, 42);
     EXPECT_EQ(T.z, 0xab);
   });
 
-  Transcode<Outer>(&F, &T, {"v", "x"}, {"v", "."}, {"v", "."});
+  transcode<Outer>(&F, &T, "v.x", "v._");
 }
 
+// TODO: GenSize does not yet exist
+#if 0
 TEST_F(PJTest, VariantNestedTagSize) {
   BigOuter F{.v = Var3{.value = {.x = 42}, .tag = Var3::Kind::x}};
   EXPECT_EQ(GenSize<BigOuter>(0x400, {"v", "x"}, {"v", "."})(&F), 9);
 }
+#endif
 
+// TODO: implement DefaultOp lowering
+#if 0
 TEST_F(PJTest, VariantDispatchDefault) {
   Outer2 F{.z = 0xab};
   Outer T{.v = {.tag = Var4::Kind::w}, .z = 0};
 
-  OnMatch<0, Outer>("undef", [&](const Outer& T) {
+  onMatch<0, Outer>("undef", [&](const Outer& T) {
     EXPECT_EQ(T.v.tag, Var4::Kind::undef);
     EXPECT_EQ(T.z, 0xab);
   });
 
-  Transcode<Outer2, Outer, Outer>(&F, &T, {}, {"v", "."}, {"v", "."});
+  transcode<Outer2, Outer, Outer>(&F, &T);
 }
+#endif
 
-// TODO: Try without path, when supported.
-TEST_F(PJTest, VariantDispatchUndef) {
+TEST_P(PJVariantTest, VariantDispatchUndef) {
   Outer F{.v = {.tag = Var4::Kind::undef}, .z = 0xab};
   Outer T{.v = {.value = {.w = 0}, .tag = Var4::Kind::w}, .z = 0};
 
-  OnMatch<0, Outer>("undef", [&](const Outer& T) {
+  onMatch<0, Outer>("v.undef", [&](const Outer& T) {
     EXPECT_EQ(T.v.tag, Var4::Kind::undef);
     EXPECT_EQ(T.z, 0xab);
   });
 
-  Transcode<Outer>(&F, &T, {"v", "undef"}, {"v", "."}, {"v", "."});
+  transcode<Outer>(&F, &T, "v.undef", "v._");
 }
 
-// TODO: try without path
-TEST_F(PJTest, VariantDifferentDispatchTag) {
+TEST_P(PJVariantTest, VariantDifferentDispatchTag) {
   Outer3 F{
       .a = {.value = {.w = 0x11}, .tag = Var4::Kind::w},
       .b = {.value = {.x = 0x22222222}, .tag = Var4::Kind::x},
@@ -242,17 +162,26 @@ TEST_F(PJTest, VariantDifferentDispatchTag) {
       .b = {.tag = Var4::Kind::undef},
   };
 
-  OnMatch<0, Outer3>("x", [&T](const Outer3& T2) {
+  onMatch<0, Outer3>("b.x", [&T](const Outer3& T2) {
     EXPECT_EQ(&T, &T2);
     EXPECT_EQ(T.a.tag, Var4::Kind::w);
-    EXPECT_EQ(T.a.value.w, T.a.value.w);
+    EXPECT_EQ(T.a.value.w, 0x11);
     EXPECT_EQ(T.b.tag, Var4::Kind::x);
-    EXPECT_EQ(T.b.value.x, T.b.value.x);
+    EXPECT_EQ(T.b.value.x, 0x22222222);
   });
 
-  Transcode<Outer3>(&F, &T, {"a", "w"}, {"a", "."}, {"b", "."});
+  transcode<Outer3>(&F, &T, "a.w", "a._");
 
-  EXPECT_EQ(GenSize<Outer3>(0x400, {"a", "w"}, {"a", "."})(&F), 11);
+  // TODO: GenSize does not yet exist
+  // EXPECT_EQ(GenSize<Outer3>(0x400, {"a", "w"}, {"a", "."})(&F), 11);
 }
+
+// Automatically tests all combinations of src_path and tag_path being provided
+// or not being provided.
+INSTANTIATE_TEST_SUITE_P(Variants, PJVariantTest,
+                         testing::Values(std::make_pair(false, false),
+                                         std::make_pair(false, true),
+                                         std::make_pair(true, false),
+                                         std::make_pair(true, true)));
 
 }  // namespace pj
