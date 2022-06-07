06ba7929877f631305de555e992811e3666abf89
Samir Jindel <samir@jindel.io>
2022-06-07 16:48:41 -0500

    Demo slides

---
+++ demo/slides/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+for slide in $(cat order); do
+  inkscape --file=$slide.svg --without-gui --export-pdf=$slide.pdf
+done
+
+(cat order; echo 'demo') | sed -e 's/$/.pdf/g' | xargs pdfunite
+
+for slide in $(cat order); do
+  rm $slide.pdf
+done
