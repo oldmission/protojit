712e3ba34994592afc406dfc1da225b26699940c
Samir Jindel <samir@jindel.io>
2022-06-17 03:10:16 -0500

    Class names in interfaces

--- pj/protogen.hpp
+++ pj/protogen.hpp
@@ -57,6 +57,9 @@ struct ParsedProtoFile {
     std::vector<Sizer> sizers;
     std::vector<Encoder> encoders;
     std::vector<Decoder> decoders;
+
+    const SourceId jit_class_name;
+    const SourceId precomp_class_name;
   };
 
   std::vector<Decl> decls;
